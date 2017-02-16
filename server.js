3f50cfce1127d98ebf6120c33b598d49e10aeb04
jangma <akkimanu2@gmail.com>
2017-02-16 18:56:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
 app.get('/article one' , function (req, res) {
     res.send ('Article one requested and will be served here');
 });  
